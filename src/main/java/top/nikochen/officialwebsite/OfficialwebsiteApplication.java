17d7aa187a1b42fcc498022f4c2e84317ecd76d8
niko <1194142140@qq.com>
2019-03-09 20:35:56 +0800

    优化

--- src/main/java/top/nikochen/officialwebsite/OfficialwebsiteApplication.java
+++ src/main/java/top/nikochen/officialwebsite/OfficialwebsiteApplication.java
@@ -2,15 +2,40 @@ package top.nikochen.officialwebsite;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @MapperScan("top.nikochen.officialwebsite")
 public class OfficialwebsiteApplication {
 
-	public static void main(String[] args) {
+    /**
+     * 跨域过滤器
+     *
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+
+    private CorsConfiguration buildConfig(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+
+
+
+    public static void main(String[] args) {
 		SpringApplication.run(OfficialwebsiteApplication.class, args);
 	}
 
